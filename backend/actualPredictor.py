adba8c0bea843e070930b68b6f4a578b7883736e
Bibodh07 <bibodh.baral07@gmail.com>
2026-06-07 23:40:18 -0400

    setting up everything for a proper predictor actually using all the data that was accumlated over the pasr

---
+++ backend/actualPredictor.py
@@ -0,0 +1,21 @@
+
+
+"""
+Goal is to make a predictor based on bayes theorm;
+Taking a win probability and then adding factors such as 
+form, player age, player rating, power, fixture etc to create an accurate prediction
+"""
+
+
+def extractData():
+    pass
+
+
+def proccessProbability():
+    pass
+
+def testProbability():
+    pass
+
+
+
