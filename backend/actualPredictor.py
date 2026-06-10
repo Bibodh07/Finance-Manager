2b9b9b96ac7174d5094fbb2c7d4b52400f20fb95
Bibodh07 <bibodh.baral07@gmail.com>
2026-06-09 23:40:08 -0400

    changes made to the probability

--- backend/actualPredictor.py
+++ backend/actualPredictor.py
@@ -4,6 +4,9 @@
 Goal is to make a predictor based on bayes theorm;
 Taking a win probability and then adding factors such as 
 form, player age, player rating, power, fixture etc to create an accurate prediction
+
+
+mathcmatical probability for form
 """
 
 
